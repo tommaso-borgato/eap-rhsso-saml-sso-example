d7fccd9c90386fc9c4ee84123af1e3e7936eb29c
Tommasso Borgato <tborgato@redhat.com>
2023-01-30 14:11:09 +0100

    SAML

---
+++ build.sh
@@ -0,0 +1,7 @@
+export SSO_URL="http://0.0.0.0:8081/realms/saml-test-realm/protocol/saml"
+export SSO_REALM="saml-test-realm"
+export SSO_USERNAME="admin"
+export SSO_PASSWORD="admin"
+export HOSTNAME_HTTP="http://localhost:8080"
+
+mvn clean package -P EAP7-1999 && ./target/server/bin/standalone.sh
